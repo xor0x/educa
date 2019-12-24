191197aea0ed03139156e24af8aefb8cb9bc8b2d
Alex Xor0x <you@example.com>
2019-12-24 08:31:14 +0200

    Add Student

--- courses/urls.py
+++ courses/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    # path('mine/', views.ManageCourseListView.as_view(), name='manage_course_list'),
-    path('', views.ManageCourseListView.as_view(), name='manage_course_list'),
+    path('mine/', views.ManageCourseListView.as_view(), name='manage_course_list'),
     path('create/', views.CourseCreateView.as_view(), name='course_create'),
     path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
     path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
@@ -19,4 +18,8 @@ urlpatterns = [
          views.ModuleContentListView.as_view(), name='module_content_list'),
     path('module/order/', views.ModuleOrderView.as_view(), name='module_order'),
     path('content/order/', views.ContentOrderView.as_view(), name='content_order'),
+    path('subject/<slug:subject>/', views.CourseListView.as_view(),
+         name='course_list_subject'),
+    path('<slug:slug>/', views.CourseDetailView.as_view(),
+         name='course_detail'),
 ]
--- django_learn/urls.py
+++ django_learn/urls.py
@@ -3,12 +3,15 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_vies
 from django.conf.urls.static import static
 from django.conf import settings
+from courses.views import CourseListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_vies.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_vies.LogoutView.as_view(), name='logout'),
     path('course/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
+    path('students/', include('students.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
