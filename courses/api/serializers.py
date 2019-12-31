5c531604b1d1d5b97f166253a41b30ff73e77b7b
Alex Xor0x <you@example.com>
2019-12-31 13:53:37 +0200

    Rest api update

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,8 +1,37 @@
 from rest_framework import serializers
-from ..models import Subject
+from ..models import Subject, Course, Module, Content
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview',
+                  'created', 'owner', 'modules']
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.rentder()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,13 @@
 from rest_framework import generics
-from ..models import Subject
-from .serializers import SubjectSerializer
+from ..models import Subject, Course
+from .serializers import SubjectSerializer, CourseSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from rest_framework.decorators import action
 
 
 class SubjectListView(generics.ListAPIView):
@@ -12,3 +19,14 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @action(methods=['post'], detail=False, authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
